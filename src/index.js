0f0774ec18e7115035f257995532879659f7973a
James <jamesclarkedublin@gmail.com>
2024-04-26 20:36:15 +0100

    Data being loaded into mongo database, returned with get request

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
 const {MongoMemoryServer} = require('mongodb-memory-server');
 const {MongoClient} = require('mongodb');
 const express = require('express');
+const fs = require('fs'); //allows file system browsing
 
 const app = express()
 
-let database = null
+const jsonFilePath = "src/data/productsSmall.json"
+const collectionName = "products"
+
+let db = null
 
 async function startDatabase() {
-    const mongo = new MongoMemoryServer();
-    const mongoDBURL = await mongo.getConnectionString();
-    const connection = await MongoClient.connect(mongoDBURL, {useNewUrlParser: true});
-    database = connection.db();
+    const mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri();
+    
+    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    db = client.db()
+
+    const dataJson = fs.readFileSync(jsonFilePath);
+    const data = JSON.parse(dataJson);
+
+    const collection = db.collection(collectionName);
+    await collection.insertMany(data);
 }
 
-
-app.get('/', async (req, res) => {
-    res.send("Hello Get!");
+app.get('/', async (req, res) => {    
+    res.send(await db.collection(collectionName).find({}).toArray());
 });
 
 app.get('/test', async (req, res) => {
     res.send("Hello test!");
 });
 
-
 app.listen(3000, async() => {
+    startDatabase();
     console.log("Listening on port 3000");
 });
